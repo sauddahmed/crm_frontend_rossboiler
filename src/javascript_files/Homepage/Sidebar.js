208ee272852c05d82483763623227faec588a154
Anis Mahat <anismahat756.am1@gmail.com>
2024-12-28 00:25:54 +0530

    CRM

--- src/javascript_files/Homepage/Sidebar.js
+++ src/javascript_files/Homepage/Sidebar.js
@@ -9,7 +9,7 @@ function Sidebar() {
     <section className="sidebar">
       <div>
         <aside>
-          <blockquote onClick={() => nav("/")}>
+          <blockquote onClick={() => nav("/dashboard")}>
             <i className="fa-solid fa-user"></i>
             <h3>Dashboard</h3>
           </blockquote>
@@ -26,52 +26,52 @@ function Sidebar() {
           </blockquote>
           {showmenu && (
             <blockquote>
-              <mark onClick={() => nav("/master/category-master")}>
+              <mark onClick={() => nav("master/category-master")}>
                 <h4>Category Master</h4>
               </mark>
-              <mark onClick={() => nav("/master/sub-category-master")}>
+              <mark onClick={() => nav("master/sub-category-master")}>
                 <h4>Sub-Category Master</h4>
               </mark>
-              <mark onClick={() => nav("/master/hsn-master")}>
+              <mark onClick={() => nav("master/hsn-master")}>
                 <h4>HSN Master</h4>
               </mark>
-              <mark onClick={() => nav("/master/unit-master")}>
+              <mark onClick={() => nav("master/unit-master")}>
                 <h4>Unit Master</h4>
               </mark>
-              <mark onClick={() => nav("/master/packing-master")}>
+              <mark onClick={() => nav("master/packing-master")}>
                 <h4>Packing Master</h4>
               </mark>
-              <mark onClick={() => nav("/master/currency-master")}>
+              <mark onClick={() => nav("master/currency-master")}>
                 <h4>Currency Master</h4>
               </mark>
               <mark>
                 <h4 style={{ color: "gray" }}>Customer Pricing</h4>
               </mark>
-              <mark onClick={() => nav("/master/gst-master")}>
+              <mark onClick={() => nav("master/gst-master")}>
                 <h4>GST Master</h4>
               </mark>
-              <mark onClick={() => nav("/master/parts-master")}>
+              <mark onClick={() => nav("master/parts-master")}>
                 <h4>Parts Master</h4>
               </mark>
-              <mark onClick={() => nav("/master/boiler-master")}>
+              <mark onClick={() => nav("master/boiler-master")}>
                 <h4>Boiler Master</h4>
               </mark>
-              <mark onClick={() => nav("/master/boiler-series-master")}>
+              <mark onClick={() => nav("master/boiler-series-master")}>
                 <h4>Boilder Series Master</h4>
               </mark>
               <mark>
                 <h4 style={{ color: "gray" }}>Boilder Series Parts Mapping</h4>
               </mark>
-              <mark onClick={() => nav("/master/courier-master")}>
+              <mark onClick={() => nav("master/courier-master")}>
                 <h4>Courier Master</h4>
               </mark>
-              <mark onClick={() => nav("/master/customer-master")}>
+              <mark onClick={() => nav("master/customer-master")}>
                 <h4>Customer Master</h4>
               </mark>
               <mark>
                 <h4 style={{ color: "gray" }}>User Management</h4>
               </mark>
-              <mark onClick={() => nav("/master/technician-master")}>
+              <mark onClick={() => nav("master/technician-master")}>
                 <h4>Technician Master</h4>
               </mark>
             </blockquote>
